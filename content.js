9260c0cf8a8b15f946a358d51681382582f33ebf
Bepa88 <leleiko.vira@gmail.com>
2022-02-09 21:36:18 +0200

    send message

--- content.js
+++ content.js
@@ -1,10 +1,31 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        alert(request.message);
+        //if (request.message === "start") {
+        //    start();
+        //}
+    }
+);
+
+function start() {
+    alert("started");
+}
+
 ////chrome.runtime.onMessage.addListener(message => {
 ////    if (message.environment) {
 ////        console.log(message.environment);
 ////    }
 ////});
 
-chrome.runtime.onMessage.addListener(function (request) {
-    console.log(request.environmentValue);
-    return true;
-});
+//chrome.runtime.onMessage.addListener(function (request) {
+//    console.log(request.environmentValue);
+//    return true;
+//});
+
+//chrome.runtime.onMessage.addListener(
+//    function (request, sender, sendResponse) {
+//        if (request.message === "start") {
+//            alert("started");
+//        }
+//    }
+//);
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-var environmentValue;
-var brandValue;
+var environmentValue = document.querySelector('input[name=Environment]:checked').value;
+var brandValue = document.querySelector('input[name=Brand]:checked').value;
 
 document.getElementById("InitializationOfVariables").addEventListener("click", InitializationOfVariables);
 
@@ -8,15 +8,34 @@ function InitializationOfVariables() {
     environmentValue = document.querySelector('input[name=Environment]:checked').value;
     brandValue = document.querySelector('input[name=Brand]:checked').value;
 
+    //chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    //    var activeTab = tabs[0];
+    //    chrome.tabs.sendMessage(activeTab.id, { "message": "start" });
+    //});
+
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, environmentValue)
     });
+
+
     //chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     //    chrome.tabs.executeScript(tabs[0].id, { file: 'content.js' }, () => {
     //        chrome.tabs.sendMessage(tabs[0].id, { environment: environmentValue });
     //    });
     //});
 }
+
+function popup() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": environmentValue });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("InitializationOfVariables").addEventListener("click", popup);
+});
 
 //function InitializationOfVariables() {
 
